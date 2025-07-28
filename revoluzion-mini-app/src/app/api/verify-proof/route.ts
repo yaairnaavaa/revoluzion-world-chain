9d14080909795361cf7c56383112040c132de851
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-07-28 00:13:31 -0700

    feat: add verification to create petition

--- revoluzion-mini-app/src/app/api/verify-proof/route.ts
+++ revoluzion-mini-app/src/app/api/verify-proof/route.ts
@@ -1,39 +1,24 @@
-import {
-  ISuccessResult,
-  IVerifyResponse,
-  verifyCloudProof,
-} from '@worldcoin/minikit-js';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server'
+import { verifyCloudProof, IVerifyResponse, ISuccessResult } from '@worldcoin/minikit-js'
 
 interface IRequestPayload {
-  payload: ISuccessResult;
-  action: string;
-  signal: string | undefined;
+	payload: ISuccessResult
+	action: string
+	signal: string | undefined
 }
 
-/**
- * This route is used to verify the proof of the user
- * It is critical proofs are verified from the server side
- * Read More: https://docs.world.org/mini-apps/commands/verify#verifying-the-proof
- */
 export async function POST(req: NextRequest) {
-  const { payload, action, signal } = (await req.json()) as IRequestPayload;
-  const app_id = process.env.NEXT_PUBLIC_APP_ID as `app_${string}`;
+	const { payload, action, signal } = (await req.json()) as IRequestPayload
+	const app_id = process.env.APP_ID as `app_${string}`
+	const verifyRes = (await verifyCloudProof(payload, app_id, action, signal)) as IVerifyResponse // Wrapper on this
 
-  const verifyRes = (await verifyCloudProof(
-    payload,
-    app_id,
-    action,
-    signal,
-  )) as IVerifyResponse; // Wrapper on this
-
-  if (verifyRes.success) {
-    // This is where you should perform backend actions if the verification succeeds
-    // Such as, setting a user as "verified" in a database
-    return NextResponse.json({ verifyRes, status: 200 });
-  } else {
-    // This is where you should handle errors from the World ID /verify endpoint.
-    // Usually these errors are due to a user having already verified.
-    return NextResponse.json({ verifyRes, status: 400 });
-  }
+	if (verifyRes.success) {
+		// This is where you should perform backend actions if the verification succeeds
+		// Such as, setting a user as "verified" in a database
+		return NextResponse.json({ verifyRes, status: 200 })
+	} else {
+		// This is where you should handle errors from the World ID /verify endpoint.
+		// Usually these errors are due to a user having already verified.
+		return NextResponse.json({ verifyRes, status: 400 })
+	}
 }
