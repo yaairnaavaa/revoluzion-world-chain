f30b417b0af24ea8a5096d4042b835467af220d8
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-07-28 00:37:09 -0700

    test handle verify

--- revoluzion-mini-app/src/app/(protected)/create-petition/page.tsx
+++ revoluzion-mini-app/src/app/(protected)/create-petition/page.tsx
@@ -4,7 +4,7 @@ import { Page } from '@/components/PageLayout';
 import { UserInfo } from '@/components/UserInfo';
 import PetitionRegistryABI from '@/abi/PetitionRegistry.json';
 import { useState, useEffect } from 'react';
-import { MiniKit } from '@worldcoin/minikit-js';
+import { MiniKit, VerifyCommandInput, VerificationLevel, ISuccessResult } from '@worldcoin/minikit-js'
 import { useWaitForTransactionReceipt } from '@worldcoin/minikit-react';
 import { createPublicClient, http } from 'viem';
 import { worldchain } from 'viem/chains';
@@ -142,8 +142,51 @@ const CreatePetitionPage = () => {
     }
   }, [isConfirmed, isConfirming, isTransactionError, transactionError, transactionId]);
 
+  const verifyPayload: VerifyCommandInput = {
+    action: 'voting-action', // This is your action ID from the Developer Portal
+    signal: '0x12312', // Optional additional data
+    verification_level: VerificationLevel.Orb, // Orb | Device
+  }
+
+  const handleVerify = async () => {
+    console.log("handleVerify 1");
+    if (!MiniKit.isInstalled()) {
+      console.log("MiniKit Not Ready");
+      return
+    }
+    console.log("handleVerify 2");
+    // World App will open a drawer prompting the user to confirm the operation, promise is resolved once user confirms or cancels
+    const {finalPayload} = await MiniKit.commandsAsync.verify(verifyPayload)
+      if (finalPayload.status === 'error') {
+        return console.log('Error payload', finalPayload)
+      }
+  
+      // Verify the proof in the backend
+      const verifyResponse = await fetch('/api/verify-proof', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
+        action: 'voting-action',
+        signal: '0x12312', // Optional
+      }),
+    })
+  
+    // TODO: Handle Success!
+    const verifyResponseJson = await verifyResponse.json()
+    if (verifyResponseJson.status === 200) {
+      console.log('Verification success!')
+    }
+  }
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if(!handleVerify()) {
+      return;
+    }
     
     console.log('Form submitted, starting validation...');
     console.log('Current form data:', formData);
