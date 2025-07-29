76f9b658a9d0a6453cc8015d825e74f5f6c09ccd
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-07-29 15:14:19 -0700

    test verify

--- revoluzion-mini-app/src/app/api/verify-proof/route.ts
+++ revoluzion-mini-app/src/app/api/verify-proof/route.ts
@@ -9,7 +9,7 @@ interface IRequestPayload {
 
 export async function POST(req: NextRequest) {
 	const { payload, action, signal } = (await req.json()) as IRequestPayload
-	const app_id = process.env.APP_ID as `app_${string}`
+	const app_id = process.env.NEXT_PUBLIC_APP_ID as `app_${string}`
 	const verifyRes = (await verifyCloudProof(payload, app_id, action, signal)) as IVerifyResponse // Wrapper on this
 
 	if (verifyRes.success) {
