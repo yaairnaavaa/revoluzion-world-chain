2b3b6fc206fa430182c74bb09bf125eb6359584f
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-07-29 14:56:40 -0700

    test verify

--- revoluzion-mini-app/src/app/(protected)/create-petition/page.tsx
+++ revoluzion-mini-app/src/app/(protected)/create-petition/page.tsx
@@ -164,7 +164,10 @@ const CreatePetitionPage = () => {
         console.log('Error payload', finalPayload);
         return false;
       }
-
+	    const app_id = process.env.APP_ID as `app_${string}`
+      console.log("app_id");
+      console.log(process.env.APP_ID);
+      console.log(app_id);
       const verifyResponse = await fetch('/api/verify-proof', {
         method: 'POST',
         headers: {
